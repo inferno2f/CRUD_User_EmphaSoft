64ae0282ed83d36a7455f4921c66df7c1c8248bb
Vlad Nikitin <nikitinv91@gmail.com>
2021-11-05 15:03:44 +0300

    custom permission

---
+++ app/api/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+
+class IsAuthUserOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.username or request.user.is_superuser
