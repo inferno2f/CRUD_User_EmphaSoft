52254a5bd411a9027679bd622b587a17aae4184f
Vlad Nikitin <nikitinv91@gmail.com>
2021-11-05 15:04:13 +0300

    imports cleaned up

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,4 @@
 from rest_framework import mixins, viewsets, permissions
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 
 from .models import User
 from .permissions import IsAuthUserOrReadOnly
